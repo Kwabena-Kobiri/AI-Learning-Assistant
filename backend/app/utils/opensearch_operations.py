beb7f49bde072e3efcb66ff318af12a395371f4b
Kwabena-Kobiri <56099212+Kwabena-Kobiri@users.noreply.github.com>
2024-10-26 13:44:47 +0000

    Update opensearch_operations.py

--- backend/app/utils/opensearch_operations.py
+++ backend/app/utils/opensearch_operations.py
@@ -11,13 +11,3 @@ opensearch_client = OpenSearch(
     max_retries=3,  # Increase the number of retries
     retry_on_timeout=True,  # Enable retries on timeout
 )
-
-# opensearch_client = OpenSearch(
-#     hosts=[{"host": "52.31.193.32", "port": 4008}],
-#     use_ssl=True,
-#     verify_certs=True,
-#     connection_class=RequestsHttpConnection,
-#     timeout=60,  # Increase the timeout to 30 seconds
-#     max_retries=3,  # Increase the number of retries
-#     retry_on_timeout=True,  # Enable retries on timeout
-# )
